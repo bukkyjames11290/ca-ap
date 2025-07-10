10993c4686865d8ef25cc3532462350640a720aa
sunshine-web-coder <bukkyjames11290@gmail.com>
2025-07-10 14:12:47 +0100

    first commit3

--- src/components/mockData/UserMockData.ts
+++ src/components/mockData/UserMockData.ts
@@ -3,13 +3,13 @@ export const mockUsers = [
     id: '1',
     name: 'Alex Johnson',
     cashtag: '$alexj',
-    profileUrl: ''
+    profileUrl: 'https://i.imgur.com/CcUPbs7.jpeg'
   },
   {
     id: '2',
     name: 'Maria Garcia',
     cashtag: '$mariag',
-    profileUrl: ''
+    profileUrl: 'https://i.imgur.com/33SJk3Q.jpeg'
   },
   {
     id: '3', 
