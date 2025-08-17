e96d65f21da383a103e4f90bad519c3217160833
sunshine-web-coder <bukkyjames11290@gmail.com>
2025-08-17 20:31:25 +0100

    dfdf2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,21 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import ProtectedRoute from "@/components/ProtectedRoute";
-import { Toaster } from "react-hot-toast";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import ProtectedRoute from '@/components/ProtectedRoute';
+import { Toaster } from 'react-hot-toast';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Credit Cards, Bank, and Loans - Personal and Business",
-  description: "Credit Cards, Bank, and Loans - Personal and Business",
+  title: '',
+  description: ''
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   return (
     <html lang="en">
       <body className={inter.className}>
